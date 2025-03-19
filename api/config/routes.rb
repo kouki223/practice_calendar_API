28a114cc8ab6d49b4a35d16c1bfcb380eed820a0
kouki223 <kouki.1818194@gmail.com>
2025-03-19 18:47:23 +0900

    予定管理をするAPIの初期設定 indexアクション（予定の一覧表示）の記述

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   namespace :auth do
     resources :sessions, only: %i[index]
   end
+
+  get '/calendar_events', to: 'calendar_events#index'
+  post '/calendar_events', to: 'calendar_events#create'
+  post '/calendar_events', to: 'calendar_events#update'
+
 end
